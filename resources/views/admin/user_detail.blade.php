8ecd594ab0c9448b298a716f29c2ae4f078b57ad
hammadisaqib@gmail.com <hammadisaqib@gmail.com>
2022-09-04 13:15:12 +0500

    work3.0 09042022

--- resources/views/admin/user_detail.blade.php
+++ resources/views/admin/user_detail.blade.php
@@ -4596,7 +4596,8 @@ License: For each use you must have a valid license purchased only from above li
 												<h2 class="fw-bold">Update Details</h2>
 												<!--end::Modal title-->
 												<!--begin::Close-->
-												<div id="kt_modal_update_customer_close" class="btn btn-icon btn-sm btn-active-icon-primary">
+												<div  class="btn btn-icon btn-sm btn-active-icon-primary">
+												<a href="{{route('user-details',$user->id)}}">
 													<!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg')}}-->
 													<span class="svg-icon svg-icon-1">
 														<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -4604,6 +4605,7 @@ License: For each use you must have a valid license purchased only from above li
 															<rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="currentColor" />
 														</svg>
 													</span>
+												</a>
 													<!--end::Svg Icon-->
 												</div>
 												<!--end::Close-->
